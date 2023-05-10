21ab4c3ae1f5cab2818739308a7a8002f0263405
szymon-zuk <szymon.p.zuk@gmail.com>
2023-05-10 19:40:40 +0200

    Dodano modele Exercise i DayName

--- trainerconnect/trainerapp/models.py
+++ trainerconnect/trainerapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
 
-# Create your models here.
+class Exercise(models.Model):
+    name = models.CharField(max_length=32)
+    sets = models.PositiveIntegerField(validators=(MinValueValidator(1), MaxValueValidator(20)))
+    reps = models.PositiveIntegerField(validators=(MinValueValidator(1), MaxValueValidator(50)))
+    load = models.PositiveIntegerField()
+    comment = models.CharField(max_length=255)
+
+
+class DayName(models.Model):
+    Days = (
+        'Poniedziałek',
+        'Wtorek',
+        'Środa',
+        'Czwartek',
+        'Piątek',
+        'Sobota',
+        'Niedziela'
+    )
+    name = models.CharField(choices=list(enumerate(Days, start=1)), max_length=32, default=1)
+
--- trainerconnect/trainerconnect/settings.py
+++ trainerconnect/trainerconnect/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "trainerapp",
+    "trainerconnect",
 ]
 
 MIDDLEWARE = [
@@ -104,16 +106,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-try:
-    from local_settings import DATABASES
-except ModuleNotFoundError:
-    print("Brak konfiguracji bazy danych w pliku local_settings.py!")
-    print("Uzupełnij dane i spróbuj ponownie!")
-    exit(0)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'trainer_app',
+        'HOST': '127.0.0.1',
+        'PASSWORD': 'coderslab',
+        'USER': 'postgres',
+        'PORT': 5432
+    }
+}
