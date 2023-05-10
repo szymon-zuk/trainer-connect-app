79abf48054a06959d7daade63b2ed9e06eeb01c8
szymon-zuk <szymon.p.zuk@gmail.com>
2023-05-10 20:14:56 +0200

    Dodano modele Training, TrainingPlan, ExerciseTraining. Przeprowadzono migracje. Dodano docstringsy do modeli.

--- trainerconnect/trainerapp/models.py
+++ trainerconnect/trainerapp/models.py
@@ -2,23 +2,71 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 
+
 class Exercise(models.Model):
+    """
+    Stores a single exercise in a plan.
+    """
+
     name = models.CharField(max_length=32)
-    sets = models.PositiveIntegerField(validators=(MinValueValidator(1), MaxValueValidator(20)))
-    reps = models.PositiveIntegerField(validators=(MinValueValidator(1), MaxValueValidator(50)))
+    sets = models.PositiveIntegerField(
+        validators=(MinValueValidator(1), MaxValueValidator(20))
+    )
+    reps = models.PositiveIntegerField(
+        validators=(MinValueValidator(1), MaxValueValidator(50))
+    )
     load = models.PositiveIntegerField()
     comment = models.CharField(max_length=255)
 
 
-class DayName(models.Model):
-    Days = (
-        'Poniedziałek',
-        'Wtorek',
-        'Środa',
-        'Czwartek',
-        'Piątek',
-        'Sobota',
-        'Niedziela'
-    )
-    name = models.CharField(choices=list(enumerate(Days, start=1)), max_length=32, default=1)
+class Training(models.Model):
+    """
+    Stores information about the training and the exercises in it.
+    Related to :model:`trainer_app.trainingplan` and :model:`trainer_app.exercise`
+    """
 
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=255)
+    exercises = models.ManyToManyField(Exercise, through="ExerciseTraining")
+
+
+class DayName(models.Model):
+    """
+    Represents the name of a day of the week. Is used in ExerciseTraining
+    to show which day does the training happen.
+    """
+
+    Days = (
+        "Poniedziałek",
+        "Wtorek",
+        "Środa",
+        "Czwartek",
+        "Piątek",
+        "Sobota",
+        "Niedziela",
+    )
+    name = models.CharField(
+        choices=list(enumerate(Days, start=1)), max_length=32, default=1
+    )
+
+
+class ExerciseTraining(models.Model):
+    """
+    Model that contains information about relation between :model:`trainer_app.training` and
+    :model:`trainer_app.exercise`.
+    """
+
+    training_id = models.ForeignKey(Training, on_delete=models.CASCADE)
+    exercise_id = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    day_name = models.ForeignKey(DayName, on_delete=models.CASCADE)
+
+
+class TrainingPlan(models.Model):
+    """
+    Stores a single training plan. Relations to :model:`auth.user` and :model:`trainer_app.training`
+    """
+
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=255)
+    trainings = models.ForeignKey(Training, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
