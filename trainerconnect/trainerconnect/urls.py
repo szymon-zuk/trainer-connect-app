e954374406c3d3a95dd954d4aeabe823a1b1658b
szymon-zuk <szymon.p.zuk@gmail.com>
2023-05-11 18:37:27 +0200

    zmieniono bazowy html

--- trainerconnect/trainerconnect/urls.py
+++ trainerconnect/trainerconnect/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from trainerapp.views import MainPage
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", MainPage.as_view(), name="main-page"),
 ]
