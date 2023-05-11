04e0d67fdfe15348edced41bf118c65846e5dee4
Szymon Żuk <116501563+szymon-zuk@users.noreply.github.com>
2023-05-11 09:27:10 +0200

    Zarejestrowano modele w panelu admina

--- trainerconnect/trainerapp/admin.py
+++ trainerconnect/trainerapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from trainerapp.models import Exercise, Training, ExerciseTraining, TrainingPlan
 
 # Register your models here.
+admin.site.register(Exercise)
+admin.site.register(Training)
+admin.site.register(ExerciseTraining)
+admin.site.register(TrainingPlan)
