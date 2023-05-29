a0655a763d6bd1a492923a607b96c6563a9e16c6
Szymon Żuk <116501563+szymon-zuk@users.noreply.github.com>
2023-05-29 11:22:45 +0200

    added EventForm for model Event

---
+++ trainerconnect/calendar_schedule/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from calendar_schedule.models import Event
+
+class EventForm(forms.ModelForm):
+
+    class Meta:
+
+        model = Event
+        fields = ['title', 'start_time', 'end_time', 'host_email', 'guest_email', 'location', 'event_description']
+        labels = {
+            'title': 'Nazwa',
+            'start_time': 'Czas rozpoczęcia',
+            'end_time': 'Czas zakończenia',
+            'host_email': 'E-mail trenera',
+            'guest_email': 'E-mail podopiecznego',
+            'location': 'Lokalizacja',
+            'event_description': "Opis spotkania"
+        }
