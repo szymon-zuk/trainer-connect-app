936ef682c00b2b79f946871f7430add8f5f7271e
Szymon Żuk <116501563+szymon-zuk@users.noreply.github.com>
2023-06-02 10:41:16 +0200

    Added tests for chat/views.py

--- trainerconnect/chat/tests.py
+++ trainerconnect/chat/tests.py
@@ -1,3 +1,70 @@
-from django.test import TestCase
+from django.test import Client
+from .models import Message, Thread
+from django.urls import reverse
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_thread_list_view_requires_login(user, client):
+    response = client.get("thread_list/")
+    assert response.status_code == 302
+    assert response.url == "/login/?next=/thread_list/"
+    client.force_login(user=user)
+    response = client.get("/thread_list/")
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_thread_list_view(user, client):
+    client.force_login(user=user)
+    response = client.get("/thread_list/")
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_thread_view_requires_login(user, client):
+    response = client.get("/add_thread/")
+    assert response.status_code == 302
+    assert response.url == "/login/?next=/add_thread/"
+    client.force_login(user=user)
+    response = client.get("/add_thread/")
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_thread_view(user, client):
+    client.force_login(user=user)
+    response = client.get("/add_thread/")
+    assert response.status_code == 200
+    initial_exercise_count = Thread.objects.count()
+    response = client.post(
+        "/add_thread/",
+        {
+            "name": "Testowa konwersacja",
+            "trainer_id": user.id,
+            "trainee_id": user.id,
+            "description": "Opis konwersacji"
+        },
+    )
+    assert response.status_code == 302
+    assert Thread.objects.count() == initial_thread_count + 1
+
+
+@pytest.mark.django_db
+def test_thread_detail_view_requires_login(user, client, thread):
+    response = client.get(
+        reverse("thread", kwargs={"pk": thread.id})
+    )
+    assert response.status_code == 302
+    client.force_login(user=user)
+    response = client.get(
+        reverse("thread", kwargs={"pk": thread.id})
+    )
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_thread_detail_view(user, client, thread):
+    client.force_login(user=user)
+    response = client.get(
+        reverse("thread", kwargs={"pk": thread.id})
+    )
+    assert response.status_code == 200
