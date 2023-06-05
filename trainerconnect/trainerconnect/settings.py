f1e4d79a6d9e72743b700437413c733b7fff9b2d
szymon-zuk <szymon.p.zuk@gmail.com>
2023-06-05 16:54:58 +0200

    hidden smtp settings

--- trainerconnect/trainerconnect/settings.py
+++ trainerconnect/trainerconnect/settings.py
@@ -53,12 +53,12 @@ LOGIN_URL = "/login/"
 
 LOGOUT_REDIRECT_URL = "/logout/"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True)
-EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = config("EMAIL_PORT", default=587)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True)
+# EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
+# EMAIL_PORT = config("EMAIL_PORT", default=587)
+# EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
 DATABASES = {
     "default": {
