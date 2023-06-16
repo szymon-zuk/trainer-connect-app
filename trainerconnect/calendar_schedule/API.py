fc8c8cbfc264bc7956f960a32e75d2e681f7adc2
Szymon Żuk <116501563+szymon-zuk@users.noreply.github.com>
2023-06-16 10:17:24 +0200

    Added API functions for editing event in Google Calendar and getting event id from API

--- trainerconnect/calendar_schedule/API.py
+++ trainerconnect/calendar_schedule/API.py
@@ -40,3 +40,43 @@ def create_event(title, start, end, location, event_description):
     }
     service.events().insert(calendarId=CAL_ID, body=new_event).execute()
     print('Event created: %s' % (new_event.get('htmlLink')))
+
+
+def get_event_id():
+    """Gets the id of last event from the table"""
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES
+    )
+    service = build("calendar", "v3", credentials=credentials)
+    event_list = service.calendarList().list().execute()
+    result = service.events().list(calendarId=CAL_ID).execute()
+    table_size = len(result['items'])
+    event_id = result['items'][table_size - 1]['id']
+    return event_id
+
+
+def edit_event(title, start, end, location, event_description, event_id):
+    """Connection same as in create_event"""
+
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES
+    )
+    service = build("calendar", "v3", credentials=credentials)
+
+    """Editing an event"""
+    timezone = "Europe/Warsaw"
+    edit_event = {
+        "summary": title,
+        "location": location,
+        "description": event_description,
+        "start": {
+            "dateTime": start.strftime("%Y-%m-%dT%H:%M:%S"),
+            "timeZone": timezone
+        },
+        "end": {
+            "dateTime": end.strftime("%Y-%m-%dT%H:%M:%S"),
+            "timeZone": timezone
+        },
+    }
+    service.events().update(calendarId=CAL_ID, eventId=event_id, body=edit_event).execute()
+    print('Event edited: %s' % (edit_event.get('htmlLink')))
