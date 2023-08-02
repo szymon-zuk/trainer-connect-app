236f950b7110e51a05d2e665e9089cbc1e9a465e
szymon-zuk <szymon.p.zuk@gmail.com>
2023-08-02 10:19:02 +0200

    Trying to setup Dockerfile and docker-compose.yml

--- trainerconnect/trainerconnect/settings.py
+++ trainerconnect/trainerconnect/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY", default="django_insecure")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False)
 
-ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = []
 
 
 # Application definition
